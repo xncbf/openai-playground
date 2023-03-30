6de3ec501e814560486a15bab24f8af092f67bf1
Joon Hwan 김준환 <xncbf12@gmail.com>
2023-03-30 16:24:48 +0900

    wip

--- main.py
+++ main.py
@@ -7,13 +7,25 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 def generate_answer(prompt):
     response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
-    max_tokens=500,
+    max_tokens=150,
+    temperature=0.7,
     messages=[
-            {"role": "system", "content": "당신은 친절한 사람이며 칭찬, 감탄사, 이모티콘도 종종 사용해요."},
-            {"role": "system", "content": "여기는 커뮤니티이며 당신은 댓글을 작성중입니다 비방, 비관적인 말 금지."},
-            {"role": "system", "content": "다른 커뮤니티는 추천하지말고 최대한 짧게 대답하세요."},
-            {"role": "system", "content": "당신은 공감봇입니다. 해결책을 제시하지 말고 다독여주고 공감 위주의 댓글을 작성하세요"},
-            {"role": "system", "content": "다나까 말투 쓰지말고 요로 끝나는 말을 사용해서 부드럽게 대답하세요."},
+            {"role": "system", "content": """
+                나는 친절한 사람이며 칭찬이나 감탄사도 많이 사용해요
+                이모티콘도 자주 사용해요.
+                음악 커뮤니티에서 대화를 하고있어요
+                나는 댓글을 작성중이에요
+                다른 커뮤니티는 추천하지않아요
+                비방, 비관적인 말, 비꼬는 말 하지않아요
+                최대한 짧게 대답해요
+                100토큰 이하로 대답해요
+                나는 익명의 유저입니다. 해결책을 제시하지 않고 다독여주고 공감 위주의 댓글을 작성해요
+                다나까 말투 쓰지않고 요로 끝나는 말을 사용해서 부드럽게 대답해요
+                상대방을 지칭할때는 님이라는 대명사를 사용해요
+                여러분이라는 말을 사용하지않아요
+                나는 락 음악을 좋아해요
+                존칭을 사용하지만 극존칭은 사용하지않아요 하실까요? 대신 할까요? 라고 사용해요
+            """},
             {"role": "user", "content": prompt}
         ]
     )
@@ -22,6 +34,8 @@ def generate_answer(prompt):
     return answer
 
 if __name__ == "__main__":
-    question = "작사, 작곡하는 프로듀서가 꿈인 고등학생이지만 요즘엔 기타에 푹 빠져서 사는 사람이라고 합니다. 6 년째 이 꿈을 갖고 살아가는 중이지만 큰 변화는 없었습니다 이렇게 이야기만 하고 실천을 잘 하지 않는 저 때문에 주변에 음악하는 사람이라도 많이 있거나 주변에서 열심히 해라 잘 하고 있다라며 다독여줄 음악인을 만나려고 가입했습니다 ☺️"
+    question = """
+    여기 계신 분들은 어떤 장르를 좋아하시는지 궁금합니다
+"""
     answer = generate_answer(question)
     print(f"질문: {question}\n답변: {answer}")
